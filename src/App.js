47f36c05d8831504b53680912efa9c6105284983
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-16 08:49:42 +0900

    doesn't work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { colors } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
+import Page1 from './components/Page1';
+// import Page2 from './components/Page2';
+// import Page3 from './components/Page3';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [route, setRoute] = useState('page1');
+  const [component, setComponent] = useState('')
+
+  const onRouteChange = (event) => {
+    const page = event.currentTarget.getAttribute('page')
+    // setRoute(page);
+    if (page === 'page1') {
+      setComponent('')
+    } else if (page === 'page2') {
+      import('./components/Page2').then((Page2) => {
+        console.log(Page2.default);
+        setComponent(Page2.default)
+      })
+    } else if (page === 'page3') {
+      import('./components/Page3').then((Page3) => {
+        setComponent(Page3.default)
+      })
+    }
+  }
+
+  // if (route === 'page1') {
+  //   return <Page1 routeChange={() => onRouteChange} />
+  // } else if (route === 'page2') {
+  //   return <Page2 routeChange={() => onRouteChange} />
+  // } else if (route === 'page3') {
+  //   return <Page3 routeChange={() => onRouteChange} />
+  // }
+  if (route === 'page1') {
+    return  <Page1 routeChange={() => onRouteChange} />
+  } else {
+    return <component  routeChange={() => onRouteChange} />
+  }
 }
 
 export default App;
---
+++ src/components/Page2.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+
+import logo from '../logo.svg';
+import '../App.css';
+
+const Page2 = ({ routeChange }) => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <ButtonGroup size="large"  variant="contained" color="primary" aria-label="contained primary button group">
+          <Button onClick={routeChange()} page='page1'>Page1</Button>
+          <Button disabled>Page2</Button>
+          <Button onClick={routeChange()} page='page3'>Page3</Button>
+        </ButtonGroup>
+      </header>
+    </div>
+  )
+}
+
+export default Page2;
